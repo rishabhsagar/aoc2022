e49cbd2ef6cb9637ae0e79d21b08d8d90c15df82
Rish <rishabh.sagar@gmail.com>
2022-12-06 12:19:56 +0000

    solved puzzle for day 06

---
+++ rish/day06/python/sol06_1.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+input_file = Path('../data/input.txt')
+
+if __name__ == "__main__":
+    with open(input_file) as file:
+        while (signal_string := file.readline().rstrip()):
+            # extracts parts of signal sequentially to identify segment
+            for seq_end in range(4, len(signal_string)):
+                seq_start = seq_end - 4
+                signal = signal_string[seq_start:seq_end]
+                print(f'Checking signal segment {signal}')
+
+                # check if the segment has repeated characters
+                if len(set(signal)) == 4:
+                    # no repeated characters
+                    print(f'Signal segement found - {signal}')
+                    print(f'starts at location: {seq_end}')
+                    break
